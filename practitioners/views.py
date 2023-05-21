fc01f7218dfa73c06d1a7b0b221b2113462ab51d
prabinshrestha737 <prabinshrestha737@gmail.com>
2023-05-22 05:44:42 +0545

    generic views for practioners create and retrive

--- practitioners/views.py
+++ practitioners/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Practitioner
+from .serializers import (
+    PractitionerSerializer,
+    )
 
-# Create your views here.
+
+class PractitionerListCreateView(generics.ListCreateAPIView):
+    queryset = Practitioner.objects.all()
+    serializer_class = PractitionerSerializer
+
+
+class PractitionerRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Practitioner.objects.all()
+    serializer_class = PractitionerSerializer
+
