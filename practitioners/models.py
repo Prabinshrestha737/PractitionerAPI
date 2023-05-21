04d10ba0159e7c8b73d06269a8a17e2e1480c73e
prabinshrestha737 <prabinshrestha737@gmail.com>
2023-05-22 05:40:49 +0545

    refactor model fields name and related name added

--- practitioners/models.py
+++ practitioners/models.py
@@ -2,15 +2,14 @@ from django.db import models
 
 
 class Practitioner(models.Model):
-    identifier = models.CharField(max_length=255)
     active = models.BooleanField(default=True)
     name = models.CharField(max_length=255)
     telecom = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
     gender = models.CharField(max_length=10)
     birth_date = models.DateField()
-    photo = models.ImageField(
-        upload_to='practitioner/photos/', null=True, blank=True)
+    deceased = models.BooleanField(default=False)
+    address = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='practitioner/photos/', null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -18,8 +17,10 @@ class Practitioner(models.Model):
 
 class Qualification(models.Model):
     practitioner = models.ForeignKey(
-            Practitioner, on_delete=models.CASCADE
-        )
+        Practitioner,
+        on_delete=models.CASCADE,
+        related_name='qualifications'
+    )
     code = models.CharField(max_length=50)
     period_start = models.DateField()
     period_end = models.DateField(null=True, blank=True)
@@ -31,8 +32,10 @@ class Qualification(models.Model):
 
 class Communication(models.Model):
     practitioner = models.ForeignKey(
-        Practitioner, on_delete=models.CASCADE
-        )
+        Practitioner,
+        on_delete=models.CASCADE,
+        related_name='communications'
+    )
     language = models.CharField(max_length=50)
     preferred = models.BooleanField(default=False)
 
